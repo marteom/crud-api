a782a5e690b0bec8097220dc43b1d660490c29b2
marteom <marteom@gmail.com>
2024-02-12 15:37:04 +0300

    fix: edit start:prod script and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,5 @@
-// const path = require('path');
-
-// module.exports = {
-//   entry: './src/server.ts',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.ts?$/,
-//         use: 'ts-loader',
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: ['.tsx', '.ts', '.js'],
-//     fallback: {
-//         "fs": false,
-//         "path": false,
-//         "os": false,
-//         "http": false,
-//         "crypto": false,
-//       }
-//   },
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   }
-// };
-
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
-
-const isMulti = process.env.NODE_ENV == 'cluster'
 
 module.exports = {
   entry: './src/server.ts',
@@ -40,9 +9,6 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   externals: [nodeExternals()],
-  // entry: isMulti
-  //   ? path.join(__dirname, 'src', 'cluster.ts')
-  //   : path.join(__dirname, 'src', 'index.ts'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
